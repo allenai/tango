514adf8faa961b10d33175f4e97076f134330652
epwalsh <epwalsh10@gmail.com>
2021-09-23 10:03:28 -0700

    add custom css

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -58,3 +58,5 @@ html_title = f"tango v{VERSION}"
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
 html_static_path = ["_static"]
+
+html_css_files = ["css/custom.css"]
