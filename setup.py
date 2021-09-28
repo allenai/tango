f38b8ddfb5487bf14ccdab0371a8e52276805f00
epwalsh <epwalsh10@gmail.com>
2021-09-28 11:06:38 -0700

    add extras to setup.py

--- setup.py
+++ setup.py
@@ -1,29 +1,36 @@
 from setuptools import setup, find_packages
 
-# Load requirements.txt with a special case for allennlp so we can handle
-# cross-library integration testing.
-with open("requirements.txt") as requirements_file:
-    import re
 
-    def fix_url_dependencies(req: str) -> str:
-        """Pip and setuptools disagree about how URL dependencies should be handled."""
-        m = re.match(
-            r"^(git\+)?(https|ssh)://(git@)?github\.com/([\w-]+)/(?P<name>[\w-]+)\.git", req
-        )
-        if m is None:
-            return req
-        else:
-            return f"{m.group('name')} @ {req}"
+def parse_requirements_file(path):
+    with open(path) as requirements_file:
+        import re
 
-    install_requirements = []
-    for line in requirements_file:
-        line = line.strip()
-        if line.startswith("#") or len(line) <= 0:
-            continue
-        install_requirements.append(fix_url_dependencies(line))
+        def fix_url_dependencies(req: str) -> str:
+            """Pip and setuptools disagree about how URL dependencies should be handled."""
+            m = re.match(
+                r"^(git\+)?(https|ssh)://(git@)?github\.com/([\w-]+)/(?P<name>[\w-]+)\.git", req
+            )
+            if m is None:
+                return req
+            else:
+                return f"{m.group('name')} @ {req}"
+
+        install_requirements = []
+        for line in requirements_file:
+            line = line.strip()
+            if line.startswith("#") or len(line) <= 0:
+                continue
+            install_requirements.append(fix_url_dependencies(line))
+
+
+# Load core requirements.
+install_requirements = parse_requirements_file("requirements.txt")
+
+# Load extra requirements.
+extras = {"dev": parse_requirements_file("dev-requirements.txt")}
 
 # version.py defines the VERSION and VERSION_SHORT variables.
-# We use exec here so we don't import cached_path whilst setting up.
+# We use exec here so we don't import `cached_path` whilst setting up.
 VERSION = {}  # type: ignore
 with open("tango/version.py", "r") as version_file:
     exec(version_file.read(), VERSION)
@@ -51,5 +58,6 @@ setup(
     ),
     entry_points={"console_scripts": ["tango=tango.__main__:main"]},
     install_requires=install_requirements,
+    extras_require=extras,
     python_requires=">=3.7.1",
 )
