fb3428d82dc5372c54a2cbc49b9926475431eb29
epwalsh <epwalsh10@gmail.com>
2021-10-07 21:43:11 -0700

    fix whitespace

--- tango/integrations/torch/__init__.py
+++ tango/integrations/torch/__init__.py
@@ -16,13 +16,13 @@ First, the setup:
 
     import torch
     import torch.nn as nn
-    
+
     from tango.common.dataset_dict import DatasetDict
     from tango.step import Step
     from tango.integrations.torch import Model
-    
+
 Now let's build and register our model:
-    
+
 .. testcode::
 
     @Model.register("basic_regression")
@@ -32,26 +32,26 @@ Now let's build and register our model:
             self.linear = nn.Linear(10, 1)
             self.sigmoid = nn.Sigmoid()
             self.mse = nn.MSELoss()
-    
+
         def forward(self, x, y=None):
             pred = self.sigmoid(self.linear(x))
             out = {"pred": pred}
             if y is not None:
                 out["loss"] = self.mse(pred, y)
             return out
-    
+
         def _to_params(self):
             return {}
-    
+
 Lastly, we'll need a step to generate data:
-    
+
 .. testcode::
 
     @Step.register("generate_data")
     class GenerateData(Step):
         DETERMINISTIC = True
         CACHEABLE = False
-    
+
         def run(self) -> DatasetDict:  # type: ignore[override]
             torch.manual_seed(1)
             return DatasetDict(
