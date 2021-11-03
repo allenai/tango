d019499bebdf933fa31ec6a6209fd156bf83f3f6
Dirk Groeneveld <dirkg@allenai.org>
2021-11-03 15:46:10 -0700

    Start fixing hard tests

--- tests/step_graph_test.py
+++ tests/step_graph_test.py
@@ -1,7 +1,40 @@
+from typing import Tuple, List
+
 import pytest
 
+from tango import Step
 from tango.common.exceptions import ConfigurationError
 from tango.step_graph import StepGraph
+
+
+@Step.register("download")
+class DownloadStep(Step[str]):
+    def run(self) -> str:
+        return "data"
+
+
+@Step.register("preprocess")
+class PreprocessStep(Step[str]):
+    def run(self, raw_data: str) -> str:
+        return raw_data + raw_data
+
+
+@Step.register("train_a")
+class TrainAStep(Step[float]):
+    def run(self, data: str) -> float:
+        return 1 / len(data)
+
+
+@Step.register("train_b")
+class TrainBStep(Step[float]):
+    def run(self, data: str) -> float:
+        return len(data)
+
+
+@Step.register("combine")
+class CombineStep(Step[Tuple[float]]):
+    def run(self, models: List[float]) -> Tuple[float]:
+        return tuple(1 / x for x in models)
 
 
 @pytest.fixture
@@ -16,11 +49,11 @@ def basic_steps():
         },
         "model_a": {
             "type": "train_a",
-            "data": {"type": "preprocessed_data", "ref": "preprocessed_data"},
+            "data": {"type": "ref", "ref": "preprocessed_data"},
         },
         "model_b": {
             "type": "train_b",
-            "data": {"type": "preprocessed_data", "ref": "preprocessed_data"},
+            "data": {"type": "ref", "ref": "preprocessed_data"},
         },
         "combined": {
             "type": "combine",
@@ -29,11 +62,11 @@ def basic_steps():
     }
 
 
-def test_parse_direct_dependencies(basic_steps):
-    assert StepGraph._parse_direct_step_dependencies(basic_steps["model_a"]) == {
+def test_find_direct_dependencies(basic_steps):
+    assert StepGraph._find_step_dependencies(basic_steps["model_a"]) == {
         "preprocessed_data"
     }
-    assert StepGraph._parse_direct_step_dependencies(basic_steps["combined"]) == {
+    assert StepGraph._find_step_dependencies(basic_steps["combined"]) == {
         "model_a",
         "model_b",
     }
@@ -50,3 +83,7 @@ def test_step_graph(basic_steps):
 def test_bad_step_graph():
     with pytest.raises(ConfigurationError):
         StepGraph({"a": {"type": "a", "b": {"type": "ref", "ref": "c"}}})
+
+
+def test_circular_reference():
+    raise NotImplementedError()  # TODO
