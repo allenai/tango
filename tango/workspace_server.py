3280491ec1eecb26325ca2e9458b25f79fd22dfd
jon borchardt <jon.borchardt@gmail.com>
2021-11-12 09:52:23 -0800

    initial code to serve and display tango viz

--- tango/workspace_server.py
+++ tango/workspace_server.py
@@ -1,20 +1,52 @@
 import logging
-from http.server import ThreadingHTTPServer, BaseHTTPRequestHandler
+from http.server import ThreadingHTTPServer, BaseHTTPRequestHandler, SimpleHTTPRequestHandler
 from threading import Thread
 from typing import Tuple
+from glob import glob
+import sys
+import json
 
 from tango.workspace import Workspace
 
-
 logger = logging.getLogger(__name__)
 
+# get json for the ui viz for a list of steps
+def run_map(run_name, workspace):
+    stepMap = workspace.registered_run(run_name)
+    steps = {stepMap.get(sub).unique_id : serialize_step(stepMap.get(sub)) for sub in list(stepMap)}
+    return steps
 
-class WorkspaceRequestHandler(BaseHTTPRequestHandler):
+def serialize_step(step):
+    return {
+        "unique_id": step.unique_id,
+        "step_name": step.step_name,
+        "step_class_name": step.step_class_name,
+        "version": step.version,
+        "dependencies": list(step.dependencies),
+        "start_time": step.start_time.isoformat(),
+        "end_time": step.end_time.isoformat(),
+        "error": step.error,
+        "result_location": step.result_location,
+        "status": step.status
+    }
+
+class WorkspaceRequestHandler(SimpleHTTPRequestHandler):
     def do_GET(self):
-        self.send_response(200)
-        self.end_headers()
-        self.wfile.write("Let's Tango!".encode("UTF-8"))
-
+        print(self.path)
+        referer = self.headers.get('Referer')
+        if referer is None:
+            self.path = 'report/index.html'
+        elif self.path == '/api/steps':
+            self.send_response(200)
+            self.send_header('Content-type', 'text/json')
+            self.end_headers()
+            output_data = {sub : run_map(sub, self.server.workspace) for sub in list(self.server.workspace.registered_runs())}
+            output_json = json.dumps(output_data)
+            self.wfile.write(output_json.encode('utf-8'))
+            return;
+        else:
+            self.path = 'report' + self.path
+        return SimpleHTTPRequestHandler.do_GET(self)
 
 class WorkspaceServer(ThreadingHTTPServer):
     def __init__(self, address: Tuple[str, int], workspace: Workspace):
