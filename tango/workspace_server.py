7bf948874c24af0fa8a5b8abb7cbb6e98c1a05d0
Dirk Groeneveld <dirkg@allenai.org>
2021-11-15 16:57:47 -0800

    Makes tests pass

--- tango/workspace_server.py
+++ tango/workspace_server.py
@@ -1,48 +1,55 @@
-import logging
-from http.server import BaseHTTPRequestHandler, SimpleHTTPRequestHandler, ThreadingHTTPServer
-from threading import Thread
-from typing import Tuple
-from glob import glob
-import sys
 import json
+import logging
+from http.server import SimpleHTTPRequestHandler, ThreadingHTTPServer
+from threading import Thread
+from typing import Any, Dict, Tuple
 
-from tango.workspace import Workspace
+from tango.workspace import StepInfo, Workspace
 
 logger = logging.getLogger(__name__)
 
-# get json for the ui viz for a list of steps
-def run_map(run_name, workspace):
-    stepMap = workspace.registered_run(run_name)
-    steps = {stepMap.get(sub).unique_id : serialize_step(stepMap.get(sub)) for sub in list(stepMap)}
-    return steps
-
-def serialize_step(step):
-    return {
-        "unique_id": step.unique_id,
-        "step_name": step.step_name,
-        "step_class_name": step.step_class_name,
-        "version": step.version,
-        "dependencies": list(step.dependencies),
-        "start_time": step.start_time.isoformat(),
-        "end_time": step.end_time.isoformat(),
-        "error": step.error,
-        "result_location": step.result_location,
-        "status": step.status
-    }
 
 class WorkspaceRequestHandler(SimpleHTTPRequestHandler):
+    @classmethod
+    def _run_map(cls, run_name: str, workspace: Workspace):
+        step_map = workspace.registered_run(run_name)
+        # TODO: This needs to return all dependencies as well.
+        return {
+            step_info.unique_id: cls._serialize_step_info(step_info)
+            for step_info in step_map.values()
+        }
+
+    @classmethod
+    def _serialize_step_info(cls, step_info: StepInfo) -> Dict[str, Any]:
+        return {
+            "unique_id": step_info.unique_id,
+            "step_name": step_info.step_name,
+            "step_class_name": step_info.step_class_name,
+            "version": step_info.version,
+            "dependencies": list(step_info.dependencies),
+            "start_time": step_info.start_time.isoformat() if step_info.start_time else None,
+            "end_time": step_info.end_time.isoformat() if step_info.end_time else None,
+            "error": step_info.error,
+            "result_location": step_info.result_location,
+            "status": step_info.status,
+        }
+
     def do_GET(self):
-        if self.path == '/':
-            self.path = 'report/index.html'
-        elif self.path == '/api/steps':
+        if self.path == "/":
+            self.path = "report/index.html"
+        elif self.path == "/api/steps":
             self.send_response(200)
-            self.send_header('Content-type', 'text/json')
+            self.send_header("Content-type", "text/json")
             self.end_headers()
-            output_data = {sub : run_map(sub, self.server.workspace) for sub in list(self.server.workspace.registered_runs())}
+            output_data = {
+                sub: self._run_map(sub, self.server.workspace)
+                for sub in list(self.server.workspace.registered_runs())
+            }
             output_json = json.dumps(output_data)
-            self.wfile.write(output_json.encode('utf-8'))
-            return;
+            self.wfile.write(output_json.encode("utf-8"))
+            return
         return SimpleHTTPRequestHandler.do_GET(self)
+
 
 class WorkspaceServer(ThreadingHTTPServer):
     def __init__(self, address: Tuple[str, int], workspace: Workspace):
