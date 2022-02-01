b697284d64614c466c2e1ee273c3e095044db522
epwalsh <petew@allenai.org>
2022-02-01 13:31:58 -0800

    format fixes

--- examples/finetune_resnet/test.py
+++ examples/finetune_resnet/test.py
@@ -2,9 +2,6 @@ from components import Model
 
 
 def test_model_from_params():
-    Model.from_params({
-        "type": "resnet_ft", 
-        "num_classes": 2, 
-        "feature_extract": True, 
-        "use_pretrained": True
-    })
+    Model.from_params(
+        {"type": "resnet_ft", "num_classes": 2, "feature_extract": True, "use_pretrained": True}
+    )
