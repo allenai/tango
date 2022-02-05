0cb352858367b95b423359973a89d8e7f6113561
Dirk Groeneveld <dirkg@allenai.org>
2022-02-04 17:23:46 -0800

    Fix test

--- tests/integrations/torch/train_test.py
+++ tests/integrations/torch/train_test.py
@@ -20,7 +20,11 @@ class TestTrainStep(TangoTestCase):
                 "test_fixtures.integrations.common",
                 "test_fixtures.integrations.torch",
             ],
-            overrides="" if with_validation else json.dumps({"steps.train.validation_split": None}),
+            overrides=""
+            if with_validation
+            else json.dumps(
+                {"steps.train.validation_split": None, "steps.train.validate_every": None}
+            ),
         )
         assert (result_dir / "train" / "data.pt").is_file()
         assert (result_dir / "train" / "work" / "state_worker0.pt").is_file()
