de7195dc0691c8a1a39e602294ef832b9e32413d
Pete <epwalsh10@gmail.com>
2022-02-07 17:04:48 -0800

    more fixes for conda-forge (#177)

--- tests/integrations/torch/train_test.py
+++ tests/integrations/torch/train_test.py
@@ -3,14 +3,20 @@ import json
 import pytest
 import torch.distributed as dist
 
+from tango.common.logging import initialize_logging, teardown_logging
 from tango.common.testing import TangoTestCase
 
 
 class TestTrainStep(TangoTestCase):
+    def setup_method(self):
+        super().setup_method()
+        initialize_logging(enable_click_logs=True)
+
     def teardown_method(self):
         super().teardown_method()
         if dist.is_initialized():
             dist.destroy_process_group()
+        teardown_logging()
 
     @pytest.mark.parametrize("with_validation", [True, False])
     def test_basic_train(self, with_validation: bool):
