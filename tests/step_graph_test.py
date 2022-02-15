283ac8d7720dc4c70669fe43df6b7b9305bb7b68
Dirk Groeneveld <dirkg@allenai.org>
2022-02-14 17:52:41 -0800

    Gotta be precise with our imports

--- tests/step_graph_test.py
+++ tests/step_graph_test.py
@@ -37,7 +37,7 @@ class TestStepGraph(TangoTestCase):
         assert [res.name for res in result] == ["step1", "step2", "step3"]
 
     def test_from_file(self):
-        import test_fixtures.package  # noqa: F401
+        import test_fixtures.package.steps  # noqa: F401
 
         step_graph = StepGraph.from_file(self.FIXTURES_ROOT / "experiment" / "hello_world.jsonnet")
         assert "hello" in step_graph
