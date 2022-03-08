7ecdbced78caf57254ea6e5bc6bd5ed6d019cab5
Akshita Bhagia <akshita23bhagia@gmail.com>
2022-03-07 16:17:48 -0800

    type info for circular import

--- tango/executor.py
+++ tango/executor.py
@@ -1,9 +1,12 @@
 import logging
-from typing import List, Optional, Set, TypeVar
+from typing import TYPE_CHECKING, List, Optional, Set, TypeVar
 
 from tango.common.util import import_extra_module
 from tango.step_graph import StepGraph
 from tango.workspace import Workspace
+
+if TYPE_CHECKING:
+    from tango.step import Step
 
 logger = logging.getLogger(__name__)
 
@@ -24,9 +27,7 @@ class Executor:
         self.workspace = workspace
         self.include_package = include_package
 
-    def execute_step(self, step, is_uncacheable_leaf_step=False):
-        # Note: did not add type information because of circular imports.
-
+    def execute_step(self, step: "Step", is_uncacheable_leaf_step: bool = False) -> None:
         # Import included packages to find registered components.
         if self.include_package is not None:
             for package_name in self.include_package:
