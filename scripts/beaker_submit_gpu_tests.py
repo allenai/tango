0100c280e827e5a43d85db43dd077ddf0b104f36
epwalsh <epwalsh10@gmail.com>
2022-04-07 14:33:32 -0700

    clean up

--- scripts/beaker_submit_gpu_tests.py
+++ scripts/beaker_submit_gpu_tests.py
@@ -5,9 +5,12 @@ Usage: python -m scripts.beaker_submit_gpu_tests NAME COMMIT_SHA
 import signal
 import sys
 
-from rich import pretty, print
+import rich
+from rich import pretty, print, traceback
 
+rich.get_console().width = max(rich.get_console().width, 160)
 pretty.install()
+traceback.install()
 
 from beaker import (  # noqa: E402
     EnvVar,
@@ -38,7 +41,7 @@ def handle_sigterm(sig, frame):
 def main(name: str, commit_sha: str):
     signal.signal(signal.SIGTERM, handle_sigterm)
 
-    print(f"Authenticated as {beaker.account.name}")
+    print(f"- Authenticated as {beaker.account.name}", end="\n\n")
 
     # Find a cluster to use. We default to using our scalable cloud cluster,
     # but we'll also check to see if we can find an on-prem cluster with enough
@@ -48,7 +51,9 @@ def main(name: str, commit_sha: str):
         for node_util in beaker.cluster.utilization(on_prem_cluster):
             if node_util.free.gpu_count is not None and node_util.free.gpu_count >= 2:
                 beaker_cluster = on_prem_cluster
-                print(f"Found on-prem cluster with enough free GPUs ({on_prem_cluster})")
+                print(
+                    f"- Found on-prem cluster with enough free GPUs ({on_prem_cluster})", end="\n\n"
+                )
                 break
         else:
             continue
@@ -69,25 +74,26 @@ def main(name: str, commit_sha: str):
         ],
     )
 
-    print("Experiment spec:", spec)
+    print("- Experiment spec:", spec.to_json(), "")
 
-    print("Submitting experiment...")
+    print("- Submitting experiment...", end="\n\n")
     experiment = beaker.experiment.create(name, spec)
     print(
-        f"Experiment {experiment.id} submitted.\nSee progress at https://beaker.org/ex/{experiment.id}"
+        f"- Experiment {experiment.id} submitted.\nSee progress at https://beaker.org/ex/{experiment.id}",
+        end="\n\n",
     )
 
     try:
-        print("Waiting for job to finish...")
+        print("- Waiting for job to finish...", end="\n\n")
         experiment = beaker.experiment.await_all(experiment, timeout=20 * 60)
 
-        print("Pulling logs...")
+        print("- Pulling logs...", end="\n\n")
         logs = "".join([line.decode() for line in beaker.experiment.logs(experiment)])
         print(logs)
 
         sys.exit(experiment.jobs[0].status.exit_code)
     except (KeyboardInterrupt, TermInterrupt):
-        print("Canceling job...")
+        print("- Canceling job...", end="\n\n")
         beaker.experiment.stop(experiment)
 
 
