eb9513df2f2560b4af5397930ac98418737f6793
epwalsh <epwalsh10@gmail.com>
2022-08-08 11:48:57 -0700

    Fix test

--- tests/integrations/beaker/executor_test.py
+++ tests/integrations/beaker/executor_test.py
@@ -2,7 +2,9 @@ import petname
 import pytest
 from beaker import DataMount
 
+from tango.common.exceptions import ConfigurationError
 from tango.common.testing import run_experiment
+from tango.executor import Executor
 from tango.integrations.beaker.executor import BeakerExecutor
 from tango.integrations.beaker.workspace import BeakerWorkspace
 from tango.settings import TangoGlobalSettings
@@ -10,19 +12,33 @@ from tango.workspaces import default_workspace
 
 
 def test_from_params(beaker_workspace_name: str):
-    executor = BeakerExecutor.from_params(
+    executor = Executor.from_params(
         dict(
+            type="beaker",
             beaker_workspace=beaker_workspace_name,
             beaker_image="ai2/conda",
             github_token="FAKE_TOKEN",
             datasets=[{"source": {"beaker": "some-dataset"}, "mount_path": "/input"}],
         ),
-        workspace=default_workspace,
+        workspace=BeakerWorkspace(beaker_workspace=beaker_workspace_name),
         clusters=["fake-cluster"],
     )
+    assert isinstance(executor, BeakerExecutor)
+    assert executor.datasets is not None
     assert len(executor.datasets) == 1
     assert isinstance(executor.datasets[0], DataMount)
     assert executor.datasets[0].source.beaker == "some-dataset"
+
+
+def test_init_with_mem_workspace(beaker_workspace_name: str):
+    with pytest.raises(ConfigurationError, match="MemoryWorkspace"):
+        BeakerExecutor(
+            workspace=default_workspace,
+            beaker_workspace=beaker_workspace_name,
+            beaker_image="ai2/conda",
+            github_token="FAKE_TOKEN",
+            clusters=["fake-cluster"],
+        )
 
 
 @pytest.fixture
