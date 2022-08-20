016209f3fa13506fab8dcb276dbeb37dbc6e85ca
Dirk Groeneveld <dirkg@allenai.org>
2022-08-19 17:42:30 -0700

    Add a test that fails

--- tests/integrations/torch/training_engine_test.py
+++ tests/integrations/torch/training_engine_test.py
@@ -1,8 +1,13 @@
+import time
+
 import pytest
 import torch
 import torch.nn as nn
+from torch.nn import MSELoss
 
+from tango.common import Lazy, DatasetDict
 from tango.common.testing import TangoTestCase
+from tango.integrations.torch import TorchTrainStep, DataLoader, TrainCallback, StopEarly
 from tango.integrations.torch.model import Model
 from tango.integrations.torch.training_engine import TorchTrainingEngine
 
@@ -39,3 +44,50 @@ class TestTorchTrainingEngine(TangoTestCase):
 
         assert (self.TEST_DIR / "worker0_grad_scaler.pt").is_file()
         assert training_engine.grad_scaler == saved_grad_scaler
+
+
+class WorseningModel(Model):
+    def __init__(self):
+        super().__init__()
+        self.linear = nn.Linear(7, 1)
+        self.loss = MSELoss()
+        self.start_time = time.time()
+
+    def forward(self, x, y):
+        y_hat = self.linear(x)
+        time.sleep(0.01)
+        return {"loss": self.loss(y_hat, y) + (time.time() - self.start_time)}
+
+
+class StopOnStepCallback(TrainCallback):
+    def __init__(self, stop_on_step: int, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.stop_on_step = stop_on_step
+
+    def post_val_loop(
+        self, step: int, epoch: int, val_metric: float, best_val_metric: float
+    ) -> None:
+        if step == self.stop_on_step:
+            raise StopEarly
+
+
+def test_with_increasing_loss():
+    model = WorseningModel()
+
+    xs = [torch.randn(7) for _ in range(100)]
+    train_set = [{"x": x, "y": x + 0.1} for x in xs]
+    dataset = DatasetDict(splits={"train": train_set, "validation": train_set}, metadata={})
+
+    step = TorchTrainStep(
+        model=model,
+        training_engine=Lazy(TorchTrainingEngine, optimizer=Lazy(torch.optim.AdamW, lr=1e-5)),
+        dataset_dict=dataset,
+        train_dataloader=Lazy(DataLoader),
+        train_steps=10,
+        validation_steps=10,
+        validate_every=7,
+        train_split="train",
+        validation_split="validation",
+        callbacks=[Lazy(StopOnStepCallback, stop_on_step=9)],
+    )
+    step.result()
