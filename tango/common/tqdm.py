71fde478172f6943c5ed30da8d4f084b2ec912ef
epwalsh <epwalsh10@gmail.com>
2022-08-30 10:47:43 -0700

    try again

--- tango/common/tqdm.py
+++ tango/common/tqdm.py
@@ -60,13 +60,14 @@ class TqdmToLogsWriter:
             file_friendly_message = replace_cr_with_newline(message)
             if file_friendly_message.strip():
                 sys.stderr.write(file_friendly_message)
+                sys.stderr.flush()
         else:
             sys.stderr.write(message)
+            sys.stderr.flush()
 
         # Every 10 seconds we also log the message.
         now = time()
         if now - self.last_message_written_time >= 10 or "100%" in message:
-            sys.stderr.flush()
             if file_friendly_message is None:
                 file_friendly_message = replace_cr_with_newline(message)
             for message in file_friendly_message.split("\n"):
