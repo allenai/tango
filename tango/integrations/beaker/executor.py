d4089acd57d903e8912a6ff9f42ad717cd618cf5
epwalsh <epwalsh10@gmail.com>
2022-09-01 14:45:35 -0700

    Fix

--- tango/integrations/beaker/executor.py
+++ tango/integrations/beaker/executor.py
@@ -460,7 +460,7 @@ class BeakerExecutor(Executor):
                         # We don't need to handle logs from Tqdm since that would result in
                         # duplicate messages (those Tqdm lines get printed directly to the output as well).
                         logging.getLogger(log_record.name).handle(log_record)
-                except:  # noqa: E722
+                except Exception:  # noqa: E722
                     # Line must not be a log record
                     if setup_stage:
                         if line_str.startswith("[TANGO] "):
