f8c1ecf877749373e631d3947a2ea5dc1969e3d1
epwalsh <epwalsh10@gmail.com>
2022-09-14 11:04:06 -0700

    Ensure base path for included modules kept in `sys.path`
    Fixes #404

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -17,6 +17,7 @@ and this project adheres to [Semantic Versioning](https://semver.org/spec/v2.0.0
 - Fixed a bug where creating a `StepInfo` object from params might result in unnecessary imports.
 - Fixed a bug where canceling the Beaker executor might not work properly.
 - Fixed a bug where the trainer trains too much when `train_epochs` is set and you're using gradient accumulation.
+- Fixed a bug where included modules might not be found when using multiprocessing when they're not on `sys.path` / `PYTHONPATH`.
 
 ## [v0.13.0](https://github.com/allenai/tango/releases/tag/v0.13.0) - 2022-09-07
 
--- tango/common/util.py
+++ tango/common/util.py
@@ -108,29 +108,32 @@ def import_module_and_submodules(package_name: str, exclude: Optional[Set[str]] 
         package_name, base_path = resolve_module_name(package_name)
     else:
         base_path = Path(".")
+    base_path = base_path.resolve()
 
     if exclude and package_name in exclude:
         return
 
     importlib.invalidate_caches()
 
-    # For some reason, python doesn't always add this by default to your path, but you pretty much
-    # always want it when using `--include-package`.  And if it's already there, adding it again at
-    # the end won't hurt anything.
-    with push_python_path(base_path):
-        # Import at top level
-        module = importlib.import_module(package_name)
-        path = getattr(module, "__path__", [])
-        path_string = "" if not path else path[0]
+    # Ensure `base_path` is first in `sys.path`.
+    if str(base_path) not in sys.path:
+        sys.path.insert(0, str(base_path))
+    else:
+        sys.path.insert(0, sys.path.pop(sys.path.index(str(base_path))))
 
-        # walk_packages only finds immediate children, so need to recurse.
-        for module_finder, name, _ in pkgutil.walk_packages(path):
-            # Sometimes when you import third-party libraries that are on your path,
-            # `pkgutil.walk_packages` returns those too, so we need to skip them.
-            if path_string and module_finder.path != path_string:  # type: ignore[union-attr]
-                continue
-            subpackage = f"{package_name}.{name}"
-            import_module_and_submodules(subpackage, exclude=exclude)
+    # Import at top level
+    module = importlib.import_module(package_name)
+    path = getattr(module, "__path__", [])
+    path_string = "" if not path else path[0]
+
+    # walk_packages only finds immediate children, so need to recurse.
+    for module_finder, name, _ in pkgutil.walk_packages(path):
+        # Sometimes when you import third-party libraries that are on your path,
+        # `pkgutil.walk_packages` returns those too, so we need to skip them.
+        if path_string and module_finder.path != path_string:  # type: ignore[union-attr]
+            continue
+        subpackage = f"{package_name}.{name}"
+        import_module_and_submodules(subpackage, exclude=exclude)
 
 
 def _parse_bool(value: Union[bool, str]) -> bool:
