544ce052e78dca4c5bbc490ed5f25ea5ec2c7988
epwalsh <epwalsh10@gmail.com>
2022-11-15 13:55:47 -0800

    Remove PyTorch Lightning integration

--- tests/integrations/pytorch_lightning/profilers_test.py
+++
@@ -1,5 +0,0 @@
-from tango.integrations.pytorch_lightning.profilers import LightningProfiler
-
-
-def test_all_profilers_registered():
-    assert "pytorch_lightning::SimpleProfiler" in LightningProfiler.list_available()
