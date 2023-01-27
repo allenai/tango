b93b36931cdde9280cd5bd5756e69e4b72e9cde1
Dirk Groeneveld <dirkg@allenai.org>
2023-01-26 16:05:29 -0800

    Test for early stopping

---
+++ tests/integrations/torch/train_callback_test.py
@@ -0,0 +1,35 @@
+from pathlib import Path
+
+import pytest
+from torch.optim import SGD
+
+from .training_engine_test import DummyModel
+from tango.common import Lazy, DatasetDict
+from tango.integrations.torch import TrainConfig, TorchTrainingEngine, Model, Optimizer, DataLoader
+from tango.integrations.torch import StopEarlyCallback, StopEarly
+from tango.workspaces import MemoryWorkspace
+
+
+def test_stop_early_callback():
+    workspace = MemoryWorkspace()
+    train_config = TrainConfig(step_id="FakeStep-abc123", work_dir=Path("/tmp"))
+    training_engine = TorchTrainingEngine(
+        train_config=train_config,
+        model=DummyModel(),
+        optimizer=Lazy(SGD, lr=0.001))
+    dataset_dict = DatasetDict(splits={"train": [1, 2, 3]})
+    train_dataloader = Lazy(DataLoader)
+
+    callback = StopEarlyCallback(
+        patience=10,
+        workspace=workspace,
+        train_config=train_config,
+        training_engine=training_engine,
+        dataset_dict=dataset_dict,
+        train_dataloader=train_dataloader
+    )
+    callback.post_val_loop(1, 1, 0.5, 0.5)
+    callback.post_val_loop(2, 1, 0.5, 0.5)
+    callback.post_val_loop(20, 1, 0.6, 0.6)
+    with pytest.raises(StopEarly):
+        callback.post_val_loop(31, 1, 0.6, 0.6)
