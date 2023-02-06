da9b97d532279b496660d059ae288be65050db57
Akshita Bhagia <akshita23bhagia@gmail.com>
2023-02-06 14:47:26 -0800

    add to import list

--- tango/steps/__init__.py
+++ tango/steps/__init__.py
@@ -3,7 +3,8 @@ Built-in :class:`~tango.step.Step` implementations that are not tied to any part
 integration.
 """
 
-__all__ = ["DatasetCombineStep", "DatasetRemixStep", "PrintStep"]
+__all__ = ["DatasetCombineStep", "DatasetRemixStep", "PrintStep", "ShellStep"]
 
 from .dataset_remix import DatasetCombineStep, DatasetRemixStep
 from .print import PrintStep
+from .shell_step import ShellStep
