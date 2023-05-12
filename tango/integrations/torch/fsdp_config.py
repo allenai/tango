f7911bd1f6d803e45fd83b8e687869af00b2b7b5
Dirk Groeneveld <dirkg@allenai.org>
2023-05-12 13:22:09 -0700

    Update FSDPConfig to match new FSDP

--- tango/integrations/torch/fsdp_config.py
+++ tango/integrations/torch/fsdp_config.py
@@ -11,50 +11,29 @@ from tango.common import FromParams
 class FSDPConfig(FromParams):
     """
     Defines all of the configurable options for Torch's :class:`~torch.distributed.fsdp.FullyShardedDataParallel`.
-
     """  # noqa: E501
 
-    reshard_after_forward: bool = True
-    """
-    See the docstring for :class:`~torch.distributed.fsdp.FullyShardedDataParallel`.
-    """
+    sharding_strategy: Optional[str] = None
 
-    move_params_to_cpu: bool = False
-    """
-    See the docstring for :class:`~torch.distributed.fsdp.FullyShardedDataParallel`.
-    """
+    cpu_offload: Optional[str] = None
 
-    move_grads_to_cpu: Optional[bool] = None
-    """
-    See the docstring for :class:`~torch.distributed.fsdp.FullyShardedDataParallel`.
+    backward_prefetch: Optional[str] = None
 
-    .. seealso::
-        :data:`move_params_to_cpu`
+    sync_module_states: bool = False
 
-    .. warning::
-        At the moment we recommend that you don't mess with this parameter, or only explicitly
-        set it to the same value as :data:`move_params_to_cpu`. If you leave it as ``None``
-        (the default), it will automatically be set to match :data:`move_params_to_cpu`.
+    forward_prefetch: bool = False
 
-        Currently training seems to crash if you set this ``False`` while :data:`move_params_to_cpu` is ``True``.
-        We're tracking `fairscale#918 <https://github.com/facebookresearch/fairscale/issues/918>`_,
-        which may be related.
-    """
+    limit_all_gathers: bool = False
 
-    mixed_precision: bool = False
+    use_orig_params: bool = False
+
+    mixed_precision: Optional[str] = None
     """
     See the docstring for :class:`~torch.distributed.fsdp.FullyShardedDataParallel`.
 
     .. important::
         We recommend setting this to the same value as the ``amp`` parameter in
         :class:`FSDPTrainingEngine`.
-
-        Based on our experiments, if you're training with AMP enabled (``amp=True``)
-        you might see a small additional speedup in training time along with a small
-        additional decrease in GPU memory utilization without any performance penalty
-        (with respect to convergence) by setting this to ``True``.
-        But if you're *not* training with AMP, setting this ``True`` could impact the
-        model's ability to converge.
 
     """
 
