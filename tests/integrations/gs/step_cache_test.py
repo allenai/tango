db0e18505541d5a1d798f30f37d4211a7393e711
Akshita Bhagia <akshita23bhagia@gmail.com>
2023-10-03 20:50:05 -0700

    gs workspaces can now be bucket subfolders

--- tests/integrations/gs/step_cache_test.py
+++ tests/integrations/gs/step_cache_test.py
@@ -1,23 +1,29 @@
 import os
 
+import pytest
+
 from tango.common.testing import TangoTestCase
 from tango.common.testing.steps import FloatStep
-from tango.integrations.gs.common import empty_bucket
+from tango.integrations.gs.common import empty_bucket_folder
 from tango.integrations.gs.step_cache import GSStepCache
 
 GS_BUCKET_NAME = os.environ.get("GS_BUCKET_NAME", "allennlp-tango-bucket")
+# GS_SUBFOLDER = f"{GS_BUCKET_NAME}/subfolder"
+GS_SUBFOLDER = f"{GS_BUCKET_NAME}/my-workspaces/workspace1"
 
 
 class TestGSStepCache(TangoTestCase):
     def setup_method(self):
         super().setup_method()
-        empty_bucket(GS_BUCKET_NAME)
+        empty_bucket_folder(GS_BUCKET_NAME)
+        empty_bucket_folder(GS_SUBFOLDER)
 
     def teardown_method(self):
         super().teardown_method()
 
-    def test_step_cache(self):
-        cache = GSStepCache(bucket_name=GS_BUCKET_NAME)
+    @pytest.mark.parametrize("gs_path", [GS_BUCKET_NAME, GS_SUBFOLDER])
+    def test_step_cache(self, gs_path):
+        cache = GSStepCache(folder_name=gs_path)
         step = FloatStep(result=1.0)
         cache[step] = 1.0
         assert step in cache
